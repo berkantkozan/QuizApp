44e3e4cd0a95878118960f874d88c2a7e248e1b5
berkantkozan <berkant.kozn@gmail.com>
2023-05-11 14:42:02 +0300

    quizzes added

--- app/index.js
+++ app/index.js
@@ -1,9 +1,31 @@
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 
-const Home = () => {
-    <View>
-        <Text>Home</Text>
-    </View>
-}
+// const Home = () => {
+//     return (
+//         <View>
+//             <Text>Home</Text>
+//         </View>
+//     )
+// }
 
-export default Home;
+// export default Home;
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import HomeScreen from '../screens/HomeScreen';
+// import StackNavigator from '../StackNavigator';
+
+// export default function App() {
+//   return (
+//    <StackNavigator/>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
